f1cad3e155df8ef414286c368c55cc588fec3c10
Dominik Rosiek <drosiek@collective-sense.com>
2016-09-14 09:11:29 +0200

    pylint clean

--- scans/executor.py
+++ scans/executor.py
@@ -85,8 +85,7 @@ class Executor(object):
         try:
             resource = http.urlopen(url)
         except URLError:
-            log.error('Cannot connect to topdis: {0}:{1}'.format(cfg.get('topdis.api.host'),
-                                                                 cfg.get('topdis.api.port')))
+            log.error('Cannot connect to topdis: %s:%s', cfg.get('topdis.api.host'), cfg.get('topdis.api.port'))
             raise TopdisConnectionException
 
         charset = resource.headers.get_content_charset() or 'utf-8'
