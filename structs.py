f32bc7d4cd2161e51c58ee9ff0927a7aa00bb492
Dominik Rosiek <drosiek@collective-sense.com>
2016-09-16 09:54:00 +0200

    get transport frotocol from protocol number

--- structs.py
+++ structs.py
@@ -39,18 +39,38 @@ class TransportProtocol(Enum):
     def from_nmap_name(cls, name):
         """
         Create TransportProtocol object basing on string name
-        Args:
-            name: string representation of transport protocol, eg. "tcp", "udp"
 
-        Returns: TransportProtocol object
+        Args:
+            name (str): string representation of transport protocol, eg. "tcp", "udp"
+
+        Returns:
+            TransportProtocol
 
         Raises: ValueError if not: tcp, udp or icmp
+
         """
         name = name.upper()
         for val in cls:
             if val.db_val == name:
                 return val
         raise ValueError('Invalid transport protocol name: %s'%name)
+
+    @classmethod
+    def from_iana(cls, number):
+        """
+        Create TransportProtocol object basing on protocol number
+
+        Args:
+            number (int): protocol number
+
+        Returns:
+            TransportProtocol
+
+        """
+        for val in cls:
+            if val.iana == number:
+                return val
+        raise ValueError('Invalid transport protocol number: %s' % number)
 
 
 class RiskLevel(Enum):
