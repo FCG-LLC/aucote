be7a359a46503c4e474eda0c6e2a6871cd67eb5d
Dominik Rosiek <drosiek@collective-sense.com>
2017-01-11 17:25:56 +0100

    fix nmap empty --script-args

--- tools/nmap/tasks/port_scan.py
+++ tools/nmap/tasks/port_scan.py
@@ -62,7 +62,7 @@ class NmapPortScanTask(CommandTask):
             for script in self.scripts.values():
                 args.append('--script')
                 args.append(script.name)
-                if script.args is not None:
+                if script.args:
                     args.append('--script-args')
                     args.append(script.args)
 
