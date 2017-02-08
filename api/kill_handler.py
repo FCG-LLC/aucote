1bbec34a7d0fc48b36c9f64707b558fb30b82167
Dominik Rosiek <drosiek@collective-sense.com>
2017-02-08 11:42:30 +0100

    Authenticate killing Aucote by API

--- api/kill_handler.py
+++ api/kill_handler.py
@@ -2,7 +2,10 @@
 Handler responsible for exit aucote application immediately
 
 """
+import hashlib
+
 from api.handler import Handler
+from aucote_cfg import cfg
 
 
 class KillHandler(Handler):
@@ -10,11 +13,16 @@ class KillHandler(Handler):
     Kills aucote
 
     """
-    def get(self):
+    def post(self):
         """
         Kills aucote
 
         Returns:
             None - kill application
         """
+        password = self.get_argument('password', None)
+        if password is None or hashlib.sha512(password.encode()).hexdigest() != cfg.get('service.api.password'):
+            self.set_status(403)
+            return
+
         self.aucote.kill()
--- tests/test_api/test_kill_handler.py
+++ tests/test_api/test_kill_handler.py
@@ -3,6 +3,7 @@ from tornado.testing import AsyncHTTPTestCase
 from tornado.web import Application
 
 from api.kill_handler import KillHandler
+from utils import Config
 
 
 class UserAPITest(AsyncHTTPTestCase):
@@ -11,6 +12,34 @@ class UserAPITest(AsyncHTTPTestCase):
         self.app = Application([('/', KillHandler, {'aucote': self.aucote})])
         return self.app
 
-    def test_user_profile_annoymous(self):
-        self.fetch('/', method='GET')
+    @patch('api.kill_handler.cfg', new_callable=Config)
+    def test_user_profile_annoymous(self, cfg):
+        cfg._cfg = {
+            'service': {
+                'api': {
+                    'password': 'ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc'
+                                '887fd67b143732c304cc5fa9ad8e6f57f50028a8ff'
+                }
+            }
+        }
+        self.fetch('/', method='POST', body='password=test')
         self.aucote.kill.assert_called_once_with()
+
+    def test_no_password(self):
+        result = self.fetch('/', method='POST', body='')
+        self.assertFalse(self.aucote.kill.called)
+        self.assertEqual(result.code, 403)
+
+    @patch('api.kill_handler.cfg', new_callable=Config)
+    def test_bad_password(self, cfg):
+        cfg._cfg = {
+            'service': {
+                'api': {
+                    'password': 'ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc'
+                                '887fd67b143732c304cc5fa9ad8e6f57f50028a8ff'
+                }
+            }
+        }
+        result = self.fetch('/', method='POST', body='password=abs')
+        self.assertFalse(self.aucote.kill.called)
+        self.assertEqual(result.code, 403)
