611b3fc71e0caba9d8f939b41f15d6695451d726
perk <marcin.stozek@gmail.com>
2017-06-29 12:17:25 +0200

    Move build script from Jenkins to repo

---
+++ .jenkins/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd $WORKSPACE/source
+
+docker build --no-cache -t cs/aucote/release .
+
+if test "${branch#*tags/}" != "$branch"; then
+	VERSION="${branch#tags/}"
+else
+	SHORT_COMMIT=`expr substr $GIT_COMMIT 1 7`
+	VERSION="0.0.0-`date -u +"%Y%m%d%H%M%S"`-$SHORT_COMMIT-$destEnv"
+fi
+
+APTLY_SERVER=http://10.12.1.225:8080
+docker run --rm -P -v `pwd`:`pwd` -w=`pwd` cs/aucote/release ./package.sh $VERSION
+curl -X POST -F file=@package/aucote_$VERSION.deb $APTLY_SERVER/api/files/aucote_$VERSION
+curl -X POST $APTLY_SERVER/api/repos/main/file/aucote_$VERSION
+ssh -tt -i ~/.ssh/aptly_rsa aptly@10.12.1.225
+echo version="$VERSION" > env.properties
+
+cd $WORKSPACE/source
+cd dockerization/hardened
+docker build --build-arg destEnv=$destEnv --no-cache -t cs/$app-hardened .
+docker tag cs/$app-hardened portus.cs.int:5000/$destEnv/cs-$app-hardened
+docker push portus.cs.int:5000/$destEnv/cs-$app-hardened
