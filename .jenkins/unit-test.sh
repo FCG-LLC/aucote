bb3189c5c603719f6fa2d878e8ea44615d7d2904
Dominik Rosiek <drosiek@collective-sense.com>
2017-07-03 09:59:15 +0200

    Use variable for source dir

--- .jenkins/unit-test.sh
+++ .jenkins/unit-test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+SOURCE_DIR=source
 docker build -t cs/aucote .
-docker run --rm -P -v `pwd`:`pwd` -w=`pwd`/source cs/aucote pylint --max-line-length=120 -f parseable $(cd source && find . -name "*.py" -not -path "./tests/*" -not -path "./venv/*") | tee pylint.out
-docker run --rm -P -v `pwd`:`pwd` -w=`pwd`/source cs/aucote nosetests --with-xunit --with-coverage --cover-erase --cover-xml --cover-package=. $(cd source && find . -maxdepth 1 -type d)
+docker run --rm -P -v `pwd`:`pwd` -w=`pwd`/${SOURCE_DIR} cs/aucote pylint --max-line-length=120 -f parseable $(cd ${SOURCE_DIR} && find . -name "*.py" -not -path "./tests/*" -not -path "./venv/*") | tee pylint.out
+docker run --rm -P -v `pwd`:`pwd` -w=`pwd`/${SOURCE_DIR} cs/aucote nosetests --with-xunit --with-coverage --cover-erase --cover-xml --cover-package=. $(cd ${SOURCE_DIR} && find . -maxdepth 1 -type d)
