f80501424382e9c1bf6bd27796f77a5228a4077b
Dominik Rosiek <drosiek@collective-sense.com>
2017-07-03 09:38:18 +0200

    Change cwd to source

--- .jenkins/unit-test.sh
+++ .jenkins/unit-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build -t cs/aucote .
-docker run --rm -P -v `pwd`:`pwd` -w=`pwd` cs/aucote pylint --max-line-length=120 -f parseable $(find . -name "*.py" -not -path "./tests/*" -not -path "./venv/*") | tee pylint.out
-docker run --rm -P -v `pwd`:`pwd` -w=`pwd` cs/aucote nosetests --with-xunit --with-coverage --cover-erase --cover-xml --cover-package=. tests/*
+docker run --rm -P -v `pwd`:`pwd` -w=`pwd`/source cs/aucote pylint --max-line-length=120 -f parseable $(cd source && find . -name "*.py" -not -path "./tests/*" -not -path "./venv/*") | tee pylint.out
+docker run --rm -P -v `pwd`:`pwd` -w=`pwd`/source cs/aucote nosetests --with-xunit --with-coverage --cover-erase --cover-xml --cover-package=. $(cd source && find . -maxdepth 1 -type d)
