a272109c6e360d85f591784b740529d871a54b95
gacopl <michal.gacek@gmail.com>
2017-07-04 09:43:01 +0200

    converting aucote

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -2,19 +2,19 @@
 
 cd $WORKSPACE/source
 
-docker build --no-cache -t cs/aucote/release .
+docker build --build-arg destEnv=$destEnv --no-cache -t cs/aucote/release .
 
 if test "${branch#*tags/}" != "$branch"; then
 	VERSION="${branch#tags/}"
 else
 	SHORT_COMMIT=`expr substr $GIT_COMMIT 1 7`
-	VERSION="0.0.0-`date -u +"%Y%m%d%H%M%S"`-$SHORT_COMMIT-$destEnv"
+	VERSION="0.0.0-`date -u +"%Y%m%d%H%M%S"`-$SHORT_COMMIT"
 fi
 
 APTLY_SERVER=http://10.12.1.225:8080
 docker run --rm -P -v `pwd`:`pwd` -w=`pwd` cs/aucote/release ./package.sh $VERSION
 curl -X POST -F file=@package/aucote_$VERSION.deb $APTLY_SERVER/api/files/aucote_$VERSION
-curl -X POST $APTLY_SERVER/api/repos/main/file/aucote_$VERSION
+curl -X POST $APTLY_SERVER/api/repos/$destEnv/file/aucote_$VERSION
 ssh -tt -i ~/.ssh/aptly_rsa aptly@10.12.1.225
 echo version="$VERSION" > env.properties
 
