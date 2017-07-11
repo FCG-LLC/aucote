19c8fcf599a3a83172cd633e007ba09566ef27e1
Dominik Rosiek <drosiek@collective-sense.com>
2017-07-11 15:07:21 +0200

    Add Tool for CVE-search

---
+++ tests/test_tools/test_cve_search/test_tool.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+from unittest.mock import MagicMock, patch
+
+from tornado.testing import gen_test, AsyncTestCase
+from structs import PhysicalPort
+from tools.cve_search.tool import CVESearchTool
+
+
+class CVESearchToolTest(AsyncTestCase):
+    def setUp(self):
+        super(CVESearchToolTest, self).setUp()
+        self.aucote = MagicMock()
+        self.exploits = MagicMock()
+        self.port = MagicMock()
+        self.config = MagicMock()
+        self.node = MagicMock()
+        self.tool = CVESearchTool(aucote=self.aucote, exploits=self.exploits, port=self.port, node=self.node,
+                                  config=self.config)
+
+    @patch('tools.cve_search.tool.CVESearchServiceTask')
+    @gen_test
+    async def test_call(self, mock_task):
+        self.assertIsNone(await self.tool())
+
+        mock_task.assert_called_once_with(aucote=self.aucote, port=self.port,
+                                          exploits=[self.aucote.exploits.find.return_value])
+        self.aucote.exploits.find.assert_called_once_with('cve-search', 'cve-search')
+
+    @patch('tools.cve_search.tool.CVESearchServiceTask')
+    @gen_test
+    async def test_call_without_port(self, mock_task):
+        self.tool.port = None
+        await self.tool()
+        self.assertIsInstance(mock_task.call_args_list[0][1]['port'], PhysicalPort)
+        self.assertEqual(mock_task.call_args_list[0][1]['port'].node, self.node)
---
+++ tools/cve_search/tool.py
@@ -0,0 +1,25 @@
+"""
+CVESearch is module which requests CVE server for vulnerabilities basing on application name and version
+
+"""
+
+from structs import PhysicalPort
+from tools.base import Tool
+from tools.cve_search.tasks import CVESearchServiceTask
+
+
+class CVESearchTool(Tool):
+    """
+    Manage vulnerability searches
+
+    """
+    def __init__(self, node=None, port=None, *args, **kwargs):
+        self.node = node
+        super(CVESearchTool, self).__init__(port=port, *args, **kwargs)
+
+    async def call(self, *args, **kwargs):
+        if not self.port:
+            self.port = PhysicalPort(node=self.node)
+
+        self.aucote.add_async_task(CVESearchServiceTask(aucote=self.aucote, port=self.port,
+                                                  exploits=[self.aucote.exploits.find('cve-search', 'cve-search')]))
