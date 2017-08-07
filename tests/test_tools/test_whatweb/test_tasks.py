c946eb82d3004f64af02dabc001d72035c66ce3c
Dominik Rosiek <drosiek@collective-sense.com>
2017-08-07 17:01:06 +0200

    Analyze whatweb output and pass to cve search

--- tests/test_tools/test_whatweb/test_tasks.py
+++ tests/test_tools/test_whatweb/test_tasks.py
@@ -1,15 +1,20 @@
 import ipaddress
-from unittest import TestCase
-from unittest.mock import MagicMock
+
+from unittest.mock import MagicMock, patch
+
+from tornado.concurrent import Future
+from tornado.testing import AsyncTestCase, gen_test
 
 from fixtures.exploits import Exploit
-from structs import Port, Node, TransportProtocol, Scan
+from structs import Port, Node, TransportProtocol, Scan, Service
 from tools.whatweb.base import WhatWebBase
+from tools.whatweb.structs import WhatWebResult, WhatWebTarget, WhatWebPlugin
 from tools.whatweb.tasks import WhatWebTask
 
 
-class WhatWebTaskTest(TestCase):
+class WhatWebTaskTest(AsyncTestCase):
     def setUp(self):
+        super(WhatWebTaskTest, self).setUp()
         self.node = Node(ip=ipaddress.ip_address('127.0.0.1'), node_id=1)
         self.port = Port(node=self.node, number=19, transport_protocol=TransportProtocol.UDP)
         self.port.protocol = 'http'
@@ -25,3 +30,77 @@ class WhatWebTaskTest(TestCase):
         result = self.task.prepare_args()
         expected = 'http://127.0.0.1:19',
         self.assertEqual(result, expected)
+
+    @patch('tools.whatweb.tasks.CommandTask.__call__')
+    @gen_test
+    async def test_call(self, mock_call):
+        plugin = WhatWebPlugin()
+        plugin.version = ['7.8']
+        plugin.name = 'PHP'
+
+        target = WhatWebTarget()
+        target.plugins = [plugin]
+
+        call_result = WhatWebResult()
+        call_result.targets = [target]
+
+        mock_call.return_value = Future()
+        mock_call.return_value.set_result(call_result)
+
+        self.task.aucote.task_mapper.assign_tasks.return_value = Future()
+        self.task.aucote.task_mapper.assign_tasks.return_value.set_result(True)
+
+        expected_service = Service(name='php', version='7.8', cpe='cpe:2.3:a:php:php:7.8:*:*:*:*:*:*:*')
+
+        await self.task()
+
+        called_services = self.task.aucote.task_mapper.assign_tasks.call_args[1]['port'].apps
+        self.assertEqual(called_services[0].cpe, expected_service.cpe)
+
+    @patch('tools.whatweb.tasks.CommandTask.__call__')
+    @gen_test
+    async def test_call_unknown_plugin(self, mock_call):
+        plugin = WhatWebPlugin()
+        plugin.version = ['7.8']
+        plugin.name = 'UnKnOwN____Pl_ugi__Nam3'
+
+        target = WhatWebTarget()
+        target.plugins = [plugin]
+
+        call_result = WhatWebResult()
+        call_result.targets = [target]
+
+        mock_call.return_value = Future()
+        mock_call.return_value.set_result(call_result)
+
+        await self.task()
+
+        self.assertFalse(self.task.aucote.task_mapper.assign_tasks.called)
+
+    @patch('tools.whatweb.tasks.CommandTask.__call__')
+    @gen_test
+    async def test_call_plugin_without_version(self, mock_call):
+        plugin = WhatWebPlugin()
+
+        target = WhatWebTarget()
+        target.plugins = [plugin]
+
+        call_result = WhatWebResult()
+        call_result.targets = [target]
+
+        mock_call.return_value = Future()
+        mock_call.return_value.set_result(call_result)
+
+        await self.task()
+
+        self.assertFalse(self.task.aucote.task_mapper.assign_tasks.called)
+
+    @patch('tools.whatweb.tasks.CommandTask.__call__')
+    @gen_test
+    async def test_call_without_result(self, mock_call):
+        mock_call.return_value = Future()
+        mock_call.return_value.set_result(None)
+
+        await self.task()
+
+        self.assertFalse(self.task.aucote.task_mapper.assign_tasks.called)
