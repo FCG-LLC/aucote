b9359e1db4595c6847d27390c392e0ebdd8c6d3e
gacopl <michal.gacek@gmail.com>
2017-08-07 03:13:25 +0200

    auto-retry on pushes to aptly and portus

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+function retry() {
+    count=$1
+    slp=$2
+    cmd=$3
+    ( for i in $(seq 0 $count); do 
+        [ $i -gt 0 ] && echo "---- Rerying $i time ----"; $cmd && break || [ $i -lt $count ] && echo "---- FAILURE, waiting $slp secs ----" && sleep $slp || exit;
+     done ) || return 1
+}
+
 
 set -ex
 
@@ -15,8 +24,8 @@ fi
 
 APTLY_SERVER=http://10.12.1.225:8080
 docker run --rm -P -v `pwd`:`pwd` -w=`pwd` cs/aucote/release ./package.sh $VERSION
-curl -X POST -F file=@package/aucote_$VERSION.deb $APTLY_SERVER/api/files/aucote_$VERSION
-curl -X POST $APTLY_SERVER/api/repos/$destEnv/file/aucote_$VERSION
+retry 5 15 "curl -X POST -F file=@package/aucote_$VERSION.deb $APTLY_SERVER/api/files/aucote_$VERSION"
+retry 5 15 "curl -X POST $APTLY_SERVER/api/repos/$destEnv/file/aucote_$VERSION"
 ssh -tt -i ~/.ssh/aptly_rsa aptly@10.12.1.225
 echo version="$VERSION" > env.properties
 
@@ -24,4 +33,4 @@ cd $WORKSPACE/source
 cd dockerization/hardened
 docker build --build-arg destEnv=$destEnv --no-cache -t cs/$app-hardened .
 docker tag cs/$app-hardened portus.cs.int:5000/$destEnv/cs-$app-hardened
-docker push portus.cs.int:5000/$destEnv/cs-$app-hardened
+retry 5 15 "docker push portus.cs.int:5000/$destEnv/cs-$app-hardened"
