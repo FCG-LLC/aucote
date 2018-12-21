bed818ac17286df2324db4af7160bc7233491a3b
Dominik Rosiek <drosiek@collective-sense.com>
2018-12-21 14:46:58 +0100

    Use int to keep end_timestamp

--- migrations/20181220_01_ZieFI-node-scan-end.py
+++ migrations/20181220_01_ZieFI-node-scan-end.py
@@ -7,5 +7,5 @@ from yoyo import step
 __depends__ = {'20181219_01_OnYTP-init-tables'}
 
 steps = [
-    step('ALTER TABLE nodes_scans add column end_timestamp bigint null;')
+    step('ALTER TABLE nodes_scans add column end_timestamp int null;')
 ]
